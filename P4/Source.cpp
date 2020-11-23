0e53489c63f9bc5f4e45a674810a10335508c146
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-23 17:05:30 +0100

    Update Source.cpp

--- P4/Source.cpp
+++ P4/Source.cpp
@@ -1,56 +1,79 @@
 #include <iostream>
-#include <time.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void show(int a[6][7]) {
-	for (int i = 0; i < 6; i++)
-	{
-		cout << "| ";
-		for (int j = 0; j < 7; j++)
-		{
-			switch (a[i][j]) {
-			case 0:
-				cout << " ";
-				break;
-			case 1:
-				cout << "X";
-				break;
-			case 2:
-				cout << "O";
-				break;
-			default:
-				break;
-			}
-			cout << " ";
-		}
-		cout << "|" << endl << "|---------------|" << endl;
-	}
-	cout << "  1 2 3 4 5 6 7  " << endl << endl;
+void show(int a [6][7]) {
+    for (int i = 0; i < 6; i++){
+        
+        std::cout << "| ";
+        for (int j = 0; j < 7; j++){
+        
+            switch (a[i][j]) {
+            case 0:
+                cout << " ";
+                break;
+            case 1:
+                cout << "X";
+                break;
+            case 2:
+                cout << "O";
+                break;
+            default:
+                break;
+            }
+            cout << " ";
+        }
+        cout << "|" << endl << "|---------------|" << endl;
+    }
+    cout << "  1 2 3 4 5 6 7  " << endl << endl;
 }
 
 int main() {
-	srand(time(NULL));
-	int tab[6][7] = { 0 };
-	show(tab);
+     
+    srand(time(NULL));
+    int tab[6][7] = { 0 };
+    show(tab);
+    
+    /* test random
+    for (int i = 0; i < 6; i++) {
+        
+        for (int j = 0; j < 7; j++) {
+            tab[i][j] = (rand() % 2) + 1;
+        }
+        
+    } */
+    
+    int colonne;
+    bool j1 = true;
+    while(1) {
+        cout << (j1?"tour j1":"tour j2");
+        cout << ", entrez un numéro de colonne : ";
+        cin >> colonne;
+        colonne--;
+        for (int i = 5; i >= 0; i--) {
+            
+            if (tab[i][colonne] == 0) {
+                if(j1) {
+                    tab[i][colonne] = 1;
+                }
+                else {
+                    tab[i][colonne] = 2;
+                    
+                }
+                //system("cls");
+                j1 = !j1;
+                break;
+            }
+            else if(i == 0) {
+                cout<<"Colonne pleine!" << endl;
+            }
 
-	//Test random
-	/*for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 7; j++) {
-			tab[i][j] = (rand() % 2) + 1;
-		}
-	}*/
-	int colonne;
-	while (1) {
-		cin >> colonne;
-		colonne--;
-		for (int i = 5; i >= 0; i--) {
-			if (tab[i][colonne] == 0) {
-				tab[i][colonne] = 1;
-				break;
-			}
-		}
-		show(tab);
-	}
-	return 0;
+        }
+        show(tab);
+        
+    }
+
+    return 0;
 }
